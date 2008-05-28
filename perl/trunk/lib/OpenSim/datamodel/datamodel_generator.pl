e90f71871f19b5646288b62754fbd155af3e4191
lulurun <lulurun@22b795fc-3f4a-0410-bee4-a5dd1d18a2ff>
2008-05-28 15:12:28 +0000

    add datamodel
    git-svn-id: https://openugai.svn.sourceforge.net/svnroot/openugai@11 22b795fc-3f4a-0410-bee4-a5dd1d18a2ff

--- perl/trunk/lib/OpenSim/datamodel/datamodel_generator.pl
+++ perl/trunk/lib/OpenSim/datamodel/datamodel_generator.pl
@@ -4,11 +4,12 @@ use strict;
 use DBHandler;
 use Data::Dump qw (dump);
 
-my $db_name = "appearance";
-my $DSN = "dbi:mysql:appearance;host=192.168.50.3;";
-my $DBUSER = "liu";
+my $db_name = "opensim";
+my $DSN = "dbi:mysql:$db_name;";
+my $DBUSER = "lulu";
 my $DBPASS = undef;
 my $LIB_PREFIX = "OpenSim::DataModel::";
+my $TEMPLATE = "Table.pm";
 
 my $res = getSimpleResult("show tables");
 my $key = "Tables_in_" . $db_name;
@@ -23,12 +24,14 @@ foreach (@$res) {
 #
 sub generateDataModel {
     my ($table, $fields) = @_;
-    my $constructor = &generateConstructor($table, $fileds);
+    my $package_name = $LIB_PREFIX . $table;
+    my $constructor = &generateConstructor($table, $fields);
     my $insert_func = &generateInsert($table, $fields);
     my $update_func = &generateUpdate($table, $fields);
     my $delete_func = &generateDelete($table, $fields);
     my $select_func = &generateSelect($table, $fields);
     my $model_tmpl = &getModelTemplate();
+    $model_tmpl =~ s/<% PACKAGE_NAME %>/$package_name/;
     $model_tmpl =~ s/<% CONSTRUCTOR %>/$constructor/;
     $model_tmpl =~ s/<% INSERT %>/$insert_func/;
     $model_tmpl =~ s/<% UPDATE %>/$update_func/;
@@ -36,6 +39,33 @@ sub generateDataModel {
     $model_tmpl =~ s/<% SELECT %>/$select_func/;
     return $model_tmpl;
 }
+
+sub generateConstructor {
+    my ($table, $fields) = @_;
+    my @member_assign = map { $_->{Field} . " => \$struct{" . $_->{Field} . "} || undef," } @$fields;
+    my $assignment = join "\n\t", @member_assign;
+
+    return << "SUB_NEW";
+sub new {
+    my \$class = shift;
+    my \$obj = shift;
+    my \%struct = ref \$obj ? \%\$obj : ();
+    my \%fields = (
+	$assignment
+	);
+    return bless \\\%fields, \$class;
+}
+SUB_NEW
+}
+
+sub generateInsert {}
+
+sub generateUpdate {}
+
+sub generateDelete {}
+
+sub generateSelect {}
+
 
 sub generateTableMapper {
     my $table = shift;
@@ -52,4 +82,9 @@ sub getSimpleResult {
     my $res = $st->exec(@sql_args);
 }
 
-
+sub getModelTemplate {
+    open(FILE, $TEMPLATE);
+    my @data = <FILE>;
+    close(FILE);
+    return join "", @data;
+}
