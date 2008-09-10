bec721da801acfb12a7e54e208d78555c8b61d6f
lulurun <lulurun@22b795fc-3f4a-0410-bee4-a5dd1d18a2ff>
2008-09-10 04:41:31 +0000

    update AvatarCreator
    git-svn-id: https://openugai.svn.sourceforge.net/svnroot/openugai@34 22b795fc-3f4a-0410-bee4-a5dd1d18a2ff

--- perl/trunk/test/AvatarCreator.pl
+++ perl/trunk/test/AvatarCreator.pl
@@ -4,27 +4,71 @@ use strict;
 use OpenUGAI::Global;
 use OpenUGAI::Utility;
 use OpenUGAI::Data::Inventory;
-use OpenUGAI::Data::User;
-use OpenUGAI::Data::Asset;
+use OpenUGAI::Data::Users;
+use OpenUGAI::Data::Assets;
+use OpenUGAI::Avatar::Appearance;
 
-my $base_dir = $ARGV[0] || "";
-opendir(DIR, $base_dir) || die "can not opendir $dir";
-my @ls = readdir(DIR);
-close(DIR);
+require "config.pl";
 
-foreach (@ls) {
-    next if ($_ eq ".");
-    next if ($_ eq "..");
-    my $sub_dir = $base_dir . "/" . $_;
-    if (-d $sub_dir) {
-	&make_avatar_dataset($sub_dir);
+my %TE_Name2IDX = (
+                   eye => 3,
+                   hair => 4,
+                   jacket_top => 13,
+                   jacket_lower => 14,
+                   pants => 2,
+                   shoes => 7,
+		   sneakers => 7, # synonym
+                   skin_face => 0,
+                   skin_upper => 5,
+                   skin_lower => 6,
+                   underwear => 17,
+		   skirt => 18,
+		   shirt => 1,
+                   );
+
+my %WearableType = (
+		    Shape => 0,
+		    Skin => 1,
+		    Hair => 2,
+		    Eyes => 3,
+		    Shirt => 4,
+		    Pants => 5,
+		    Shoes => 6,
+		    Socks => 7,
+		    Jacket => 8,
+		    Gloves => 9,
+		    Undershirt => 10,
+		    Underpants => 11,
+		    Skirt => 12,
+		    );
+
+# real
+#&batch();
+
+# test
+my $base_dir = $ARGV[0] || die "no dir";
+&make_avatar_dataset($base_dir);
+
+sub batch {
+    my $base_dir = $ARGV[0] || "";
+    opendir(DIR, $base_dir) || die "can not opendir $base_dir";
+    my @ls = readdir(DIR);
+    close(DIR);
+    
+    foreach (@ls) {
+	next if ($_ eq ".");
+	next if ($_ eq "..");
+	my $sub_dir = $base_dir . "/" . $_;
+	if (-d $sub_dir) {
+	    &make_avatar_dataset($sub_dir);
+	}
     }
 }
 
 sub make_avatar_dataset {
     my $folder = shift;
     # make user
-    my $user_id = &OpenUGAI::GenerateUUID();
+    my $user_id = &OpenUGAI::Utility::GenerateUUID();
     $folder =~ /([^\/]+)$/;
     my $first_name = $1;
     my $last_name = "tmpl";
@@ -54,10 +98,10 @@ sub make_avatar_dataset {
 		    "profileFirstImage" => "",
 		    "webLoginKey" => "",
 		    );
-	&OpenUGAI::Data::User::createUser(\%user);
+	&OpenUGAI::Data::Users::createUser(\%user);
     }
     # make inventory folder
-    my $root_folder_id = &OpenUGAI::GenerateUUID();
+    my $root_folder_id = &OpenUGAI::Utility::GenerateUUID();
     {
 	my %inventory_folder = (
 				"folderName" => "My Inventory",
@@ -69,13 +113,25 @@ sub make_avatar_dataset {
 				);
 	&OpenUGAI::Data::Inventory::saveInventoryFolder(\%inventory_folder);
     }
-    my $avatar_folder_id = &OpenUGAI::GenerateUUID();
+    my $wearables_folder_id = &OpenUGAI::Utility::GenerateUUID();
     {
 	my %inventory_folder = (
-				"folderName" => "Avatar",
+				"folderName" => "Wearables",
 				"type" => -1,
 				"version" => 1,
-				"folderID" => $avatar_folder_id,
+				"folderID" => $wearables_folder_id,
+				"agentID" => $user_id,
+				"parentFolderID" => $root_folder_id,
+				);
+	&OpenUGAI::Data::Inventory::saveInventoryFolder(\%inventory_folder);
+    }
+    my $images_folder_id = &OpenUGAI::Utility::GenerateUUID();
+    {
+	my %inventory_folder = (
+				"folderName" => "Images",
+				"type" => -1,
+				"version" => 1,
+				"folderID" => $images_folder_id,
 				"agentID" => $user_id,
 				"parentFolderID" => $root_folder_id,
 				);
@@ -85,34 +141,50 @@ sub make_avatar_dataset {
     opendir(DIR, $folder) || die "can not opendir $folder";
     my @ls = readdir(DIR);
     closedir(DIR);
+
+    my %teidx2uuid = ();
+    my $assetset_xml = "";
     foreach (@ls) {
 	next if ($_ eq ".");
 	next if ($_ eq "..");
-	my $filename = $folder . $_;
+	next if ($_ =~ /\.xml$/);
+	my $filename = $folder . "/" .$_;
 	my ($asset_name, undef) = split(/\./, $_);
-	my $asset_id = &OpenUGAI::GenerateUUID();
+	my $asset_id = &OpenUGAI::Utility::GenerateUUID();
 	my $asset_data = &load_asset_data($filename);
+	my $asset_type = 0;
+	my $inv_type = 0;
+	my $parent_folder_id = $images_folder_id;
+	if ($_ =~ /\.jp2$/) {
+	    $teidx2uuid{&guess_texture_index($_)} = $asset_id;
+	}
+	if ($_ =~ /\.dat$/) {
+	    $parent_folder_id = $wearables_folder_id;
+	    $inv_type = 18;
+	    $asset_type = &guess_asset_type($_);
+	}
 	{
 	    my %asset = (
 			 "name" => $asset_name,
 			 "description" => "",
-			 "assetType" => 0,
+			 "assetType" => $asset_type,
 			 "local" => 0,
 			 "temporary" => 0,
 			 "data" => $asset_data,
 			 "id" => $asset_id,
 			 );
-	    &OpenUGAI::Data::Asset::UpdateAsset(\%asset);
+	    $assetset_xml .= &make_assetset_section(\%asset, $_);
+	    #&OpenUGAI::Data::Assets::UpdateAsset(\%asset);
 	}
 	{
 	    my %inv_item = (
 			    "assetID" => $asset_id,
-			    "assetType" => 0,
+			    "assetType" => $asset_type,
 			    "inventoryName" => $asset_name,
 			    "inventoryDescription" => "",
 			    "inventoryNextPermissions" => 532480,
 			    "inventoryCurrentPermissions" => 2147483647,
-			    "invType" => 0,
+			    "invType" => $inv_type,
 			    "creatorID" => $user_id,
 			    "inventoryBasePermissions" => 2147483647,
 			    "inventoryEveryOnePermissions" => 0,
@@ -122,21 +194,102 @@ sub make_avatar_dataset {
 			    "groupID" => &OpenUGAI::Utility::ZeroUUID,
 			    "groupOwned" => 0,
 			    "flags" => 0,
-			    "inventoryID" => &OpenUGAI::GenerateUUID(),
+			    "inventoryID" => &OpenUGAI::Utility::GenerateUUID(),
 			    "avatarID" => $user_id,
-			    "parentFolderID" => $avatar_folder_id,
+			    "parentFolderID" => $parent_folder_id,
 			    );
 	    &OpenUGAI::Data::Inventory::saveInventoryItem(\%inv_item);
 	}
     } # foreach (@ls)
+
+    # reassign textures
+    foreach (@ls) {
+	next if ($_ !~ /\.dat$/);
+	&reassgin_wearable_textures($_, \%teidx2uuid, $folder);	
+    }
+
+    $assetset_xml = "<Nini>\n$assetset_xml</Nini>\n";
+    open(FILE, ">$base_dir/assetset.xml");
+    print FILE $assetset_xml;
+    close(FILE);
+}
+
+sub make_assetset_section {
+    my ($asset, $filename) = @_;
+    my $xml = << "XML_SECTION";
+ <Section Name="$asset->{name}">
+    <Key Name="assetID" Value="$asset->{id}" />
+    <Key Name="name" Value="$asset->{name}" />
+    <Key Name="assetType" Value="$asset->{assetType}" />
+    <Key Name="inventoryType" Value="0" />
+    <Key Name="fileName" Value="$filename" />
+  </Section>
+XML_SECTION
+    return $xml;
 }
 
 sub load_asset_data {
     my $filename = shift;
-    open(FH,'<'.$filename);
+    my $bin = "";
+    open(FH,'<'.$filename) || die "can not open $filename";
     binmode(FH);
-    $size = read(FH,$bin,-s $filename);
+    my $size = read(FH,$bin,-s $filename);
     print "INFO: load asset $filename data size: $size\n";
     close(FH);
+    return $bin;
+}
+
+sub guess_asset_type {
+    my $filename = shift;
+    foreach (keys %WearableType) {
+        my $pattern = $_;
+        if ($filename =~ /$pattern/i) {
+	    my $asset_type = $WearableType{$pattern} > 3 ? 5 : 13;
+            print "Guess $asset_type $filename\n";
+            return $asset_type;
+        }
+    }
+    # must die
+    die "WARN guess failed for $filename";
+}
+
+sub guess_texture_index {
+    my $filename = shift;
+    foreach (keys %TE_Name2IDX) {
+        my $pattern = $_;
+        my $index = $TE_Name2IDX{$pattern};
+        if ($filename =~ /$pattern/i) {
+            print "Guess $index $filename\n";
+            return $index;
+        }
+    }
+    # must die
+    die "WARN guess failed for $filename";
+}
+
+sub reassgin_wearable_textures {
+    my ($filename, $hash_map, $folder) = @_;
+    my $app_file = "$folder/$filename";
+    my $appearance = &ParseLLWearableFile($app_file);
+    my $textures = $appearance->{textures};
+    foreach (keys %$textures) {
+	next if ($_ eq "header");
+        $textures->{$_} = $hash_map->{$_};
+    }
+    $appearance->{textures} = $textures;
+    # update appearance file
+    open(FILE, ">$app_file") || die "can not save $app_file";
+    print FILE $appearance->ToLLFormat();
+    close(FILE);
+}
+
+sub ParseLLWearableFile {
+    my $file = shift;
+    open(FILE, $file) || die "can not open $file";
+    my @data = <FILE>;
+    close(FILE);
+    my $appearance_string = join "", @data;
+    my $appearance = new OpenUGAI::Avatar::Appearance($appearance_string);
+    return $appearance;
 }
 
