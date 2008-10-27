b67dc1556111f42b892fd6b6118605d7799e9138
lulurun <lulurun@22b795fc-3f4a-0410-bee4-a5dd1d18a2ff>
2008-10-27 12:46:21 +0000

    git-svn-id: https://openugai.svn.sourceforge.net/svnroot/openugai@50 22b795fc-3f4a-0410-bee4-a5dd1d18a2ff

--- perl/trunk/config.pl
+++ perl/trunk/config.pl
@@ -2,22 +2,24 @@
 
 use OpenUGAI::Global;
 
+my $base_url = "http://openugai.wolfdrawer.net/perl/trunk";
+my $base_path = "/srv/www/openugai";
+
 # grid services
-$OpenUGAI::Global::USER_SERVER_URL = "http://openugai.wolfdrawer.net/perl/trunk/user.cgi";
-$OpenUGAI::Global::GRID_SERVER_URL = "http://openugai.wolfdrawer.net/perl/trunk/grid.cgi";
-$OpenUGAI::Global::ASSET_SERVER_URL = "http://openugai.wolfdrawer.net/perl/trunk/asset.cgi";
-$OpenUGAI::Global::INVENTORY_SERVER_URL = "http://openugai.wolfdrawer.net/perl/trunk/inventory.cgi";
+$OpenUGAI::Global::USER_SERVER_URL = $base_url . "/user.cgi";
+$OpenUGAI::Global::GRID_SERVER_URL = $base_url . "/grid.cgi";
+$OpenUGAI::Global::ASSET_SERVER_URL = $base_url . "/asset.cgi";
+$OpenUGAI::Global::INVENTORY_SERVER_URL = $base_url . "/inventory.cgi";
 
 # log files
-$OpenUGAI::Global::LOGDIR = "/srv/www/openugai/logs";
-$OpenUGAI::Global::TMPLDIR = "/srv/www/openugai/perl/trunk/template";
-$OpenUGAI::Global::LOGINKEYDIR = "/srv/www/openugai/perl/trunk/loginkey";
+$OpenUGAI::Global::LOGDIR = $base_path . "/logs";
+$OpenUGAI::Global::TMPLDIR = $base_path . "/perl/trunk/template";
+$OpenUGAI::Global::LOGINKEYDIR = $base_path . "/perl/trunk/loginkey";
 
 # db settings
 $OpenUGAI::Global::DSN = "dbi:mysql:openugai;host=localhost;";
 $OpenUGAI::Global::DBUSER = "opensim";
 $OpenUGAI::Global::DBPASS = "opensim";
-
 
 1;
 
